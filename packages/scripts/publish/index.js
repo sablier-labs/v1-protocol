4a1308c0db40a55d3edc007c07252ef918b8e271
Paul Berg <hello@paulrberg.com>
2019-05-24 02:04:31 +0100

    refactor: temporarily nuke out the scripts package

--- packages/scripts/publish/index.js
+++
@@ -1,8 +0,0 @@
-const multirelease = require("multi-semantic-release");
-
-// prettier-ignore
-multirelease([
-    `${__dirname}/../../website/package.json`,
-    `${__dirname}/../../contracts/package.json`,
-    `${__dirname}/../../dev-utils/package.json`
-]);
