650697b54fead818920bd68383cc66b8bd33d18f
Paul Berg <hello@paulrberg.com>
2019-08-16 03:19:58 +0300

    feat(eslint-config): initial commit

---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,52 @@
+module.exports = {
+  extends: ["prettier", "prettier/react"],
+  rules: {
+    "arrow-body-style": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    "func-names": ["error", "as-needed"],
+    "import/no-dynamic-require": "off",
+    "import/no-extraneous-dependencies": "off",
+    indent: [
+      "error",
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    "jsx-a11y/control-has-associated-label": "off",
+    "linebreak-style": "off",
+    "max-len": [
+      "warn",
+      120,
+      {
+        ignoreComments: true,
+      },
+      {
+        ignoreTrailingComments: true,
+      },
+    ],
+    "no-console": "off",
+    "no-trailing-spaces": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+    "no-underscore-dangle": [
+      "error",
+      {
+        allow: ["_id", "__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"],
+      },
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "_",
+        varsIgnorePattern: "_",
+      },
+    ],
+    "prefer-template": "off",
+    quotes: ["error", "double"],
+    strict: "off",
+  },
+};
