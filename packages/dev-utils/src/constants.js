65bff31f039daefff2ab332115d591b9cb44a2a4
Paul Berg <hello@paulrberg.com>
2019-08-20 01:14:07 +0300

    refactor(dev-utils): rename STANDARD_DEPOSIT to STANDARD_SALARY

--- packages/dev-utils/src/constants.js
+++ packages/dev-utils/src/constants.js
@@ -1,15 +1,15 @@
 const BigNumber = require("bignumber.js");
 
-const STANDARD_DEPOSIT = new BigNumber(3600).multipliedBy(1e18);
+const STANDARD_SALARY = new BigNumber(3600).multipliedBy(1e18);
 
 module.exports = {
   FIVE_UNITS: new BigNumber(5).multipliedBy(1e18),
   GAS_LIMIT: 6721975,
-  INITIAL_SUPPLY: STANDARD_DEPOSIT.multipliedBy(1000),
+  INITIAL_SUPPLY: STANDARD_SALARY.multipliedBy(1000),
   ONE_UNIT: new BigNumber(1).multipliedBy(1e18),
   RPC_URL: "http://127.0.0.1:8545",
   RPC_PORT: 8545,
-  STANDARD_DEPOSIT,
+  STANDARD_SALARY,
   STANDARD_RATE: new BigNumber(1).multipliedBy(1e18),
   STANDARD_TIME_DELTA: new BigNumber(3600),
   STANDARD_TIME_OFFSET: new BigNumber(300),
