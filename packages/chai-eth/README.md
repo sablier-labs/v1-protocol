a27935e9d4811069cba3a41a85395a9a30ad5318
Paul Berg <hello@paulrberg.com>
2019-09-01 22:58:50 +0300

    feat(chai-eth): initial commit

---
+++ packages/chai-eth/README.md
@@ -0,0 +1,46 @@
+## Chai Eth
+
+Chai plugin for ethereum smart contracts.
+
+## Usage
+
+Install the module:
+
+```bash
+$ yarn add @sablier/dev-utils
+```
+
+And import it in your project:
+
+```js
+const chaiEth = require("@sablier/chai-eth");
+```
+
+## Contributing
+
+We highly encourage participation from the community to help shape the development of Sablier. If you are interested in
+contributing or have any questions, please ping us on [Twitter](https://twitter.com/SablierHQ).
+
+### Install Modules
+
+```bash
+$ yarn install
+```
+
+### Build
+
+```bash
+$ yarn build
+```
+
+### Lint
+
+```bash
+$ yarn lint
+```
+
+### Clean
+
+```bash
+$ yarn clean
+```
---
+++ packages/chai-eth/src/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable func-names, no-else-return, no-param-reassign */
+const BigNumber = require("bignumber.js");
+
+module.exports = (chai, _utils) => {
+  // see https://twitter.com/nicksdjohnson/status/1132394932361023488
+  const blockTimeAverage = new BigNumber(14).multipliedBy(1e18);
+
+  const convert = (value) => {
+    let number;
+
+    if (typeof value === "string" || typeof value === "number") {
+      number = new BigNumber(value);
+      console.log("hey world");
+    } else if (BigNumber.isBigNumber(value)) {
+      number = value;
+    } else {
+      new chai.Assertion(value).assert(false, `expected ${value} to be an instance of string, number or BigNumber`);
+    }
+
+    return number;
+  };
+
+  /**
+   * Tolerate a larger set of values instead of just one. In real life, it can take up to 14 seconds
+   * for a block to be broadcast on the Ethereum network, so we have to account for this.Note that we
+   * make two assumptions:
+   *
+   * 1. The payment rate is 1 token/ second, which is true for all tests in this repo.
+   * 2. The token has 18 decimals
+   */
+  chai.Assertion.addMethod("tolerateTheBlockTimeVariation", function(expected, addTheBlockTimeAverage = true) {
+    const actual = convert(this._obj);
+    expected = convert(expected);
+
+    if (addTheBlockTimeAverage) {
+      const expectedCeiling = actual.plus(blockTimeAverage);
+
+      return this.assert(
+        expected.isGreaterThanOrEqualTo(actual) && expected.isLessThanOrEqualTo(expectedCeiling),
+        `expected ${expected.toString()} to be >= than ${actual.toString()} and <= ${expectedCeiling.toString()}`,
+      );
+    } else {
+      const expectedFloor = actual.minus(blockTimeAverage);
+
+      return this.assert(
+        expected.isLessThanOrEqualTo(actual) && expected.isGreaterThanOrEqualTo(expectedFloor),
+        `expected ${expected.toString()} to be <= than ${actual.toString()} and >= ${expectedFloor.toString()}`,
+      );
+    }
+  });
+};
