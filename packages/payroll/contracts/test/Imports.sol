d76ea59194357c3437581deb99979dd5db64ba0b
Paul Berg <hello@paulrberg.com>
2019-09-08 20:25:00 +0300

    refactor(payroll): move mocks to their own folder
    also delete NonStandardIERC20

--- packages/payroll/contracts/test/Imports.sol
+++ packages/payroll/contracts/test/Imports.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.5.10;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
+import "@sablier/protocol/contracts/Sablier.sol";
 
 // You might think this file is a bit odd, but let me explain.
 // We only use some contracts in our tests, which means Truffle
