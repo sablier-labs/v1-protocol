b31122d4e03360931a373fab681d8e0af6b19b57
Paul Berg <hello@paulrberg.com>
2019-09-12 01:31:33 +0300

    build(protocol): add evilERC20 and nonStandardERC20 to trf migrations

---
+++ packages/protocol/migrations/2_deploy_tokens.js
@@ -0,0 +1,20 @@
+/* global artifacts, web3 */
+const ERC20Mock = artifacts.require("./ERC20Mock.sol");
+const EvilERC20 = artifacts.require("./EvilERC20.sol");
+const NonStandardERC20 = artifacts.require("./NonStandardERC20.sol");
+
+module.exports = (deployer, _, accounts) => {
+  const initialBalance = web3.utils.toWei("7200");
+
+  deployer.deploy(ERC20Mock).then(async (token) => {
+    await token.mint(accounts[0], initialBalance);
+  });
+
+  deployer.deploy(EvilERC20).then(async (evilToken) => {
+    await evilToken.mint(accounts[0], initialBalance);
+  });
+
+  deployer.deploy(NonStandardERC20).then(async (nonStandardToken) => {
+    await nonStandardToken.nonStandardMint(accounts[0], initialBalance);
+  });
+};
