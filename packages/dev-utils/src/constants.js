93d830351f489a4de7b92347ce2dbced583fca84
Paul Berg <hello@paulrberg.com>
2019-09-16 04:59:06 +0300

    feat(dev-utils): change two constants values
    EXCHANGE_RATE_BLOCK_DELTA from 1e18 to 1e24
    INITIAL_EXCHANGE_RATE from 2e27 to 2e26

--- packages/dev-utils/src/constants.js
+++ packages/dev-utils/src/constants.js
@@ -3,11 +3,11 @@ const BigNumber = require("bignumber.js");
 const STANDARD_SALARY = new BigNumber(3600).multipliedBy(1e18);
 
 module.exports = {
-  EXCHANGE_RATE_BLOCK_DELTA: new BigNumber(1e18),
+  EXCHANGE_RATE_BLOCK_DELTA: new BigNumber(1e24),
   FIVE_UNITS: new BigNumber(5).multipliedBy(1e18),
   FIVE_UNITS_CTOKEN: new BigNumber(5).multipliedBy(1e8),
   GAS_LIMIT: 6721975,
-  INITIAL_EXCHANGE_RATE: new BigNumber(2e27),
+  INITIAL_EXCHANGE_RATE: new BigNumber(2e26),
   INITIAL_SUPPLY: STANDARD_SALARY.multipliedBy(1000),
   ONE_UNIT: new BigNumber(1).multipliedBy(1e18),
   ONE_UNIT_CTOKEN: new BigNumber(1).multipliedBy(1e8),
