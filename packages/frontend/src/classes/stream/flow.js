2a2fa2d06065506ce3fe87ebfdab626a49907b53
Paul Razvan Berg <hello@paulrberg.com>
2019-09-27 00:41:39 +0300

    refactor: remove frontend app from the monorepo

--- packages/frontend/src/classes/stream/flow.js
+++
@@ -1,6 +0,0 @@
-import { Enum } from "enumify";
-
-class StreamFlow extends Enum {}
-StreamFlow.initEnum(["UNDEFINED", "IN", "OUT"]);
-
-export default StreamFlow;
--- packages/frontend/src/classes/stream/status.js
+++
@@ -1,6 +0,0 @@
-import { Enum } from "enumify";
-
-class StreamStatus extends Enum {}
-StreamStatus.initEnum(["UNDEFINED", "CREATED", "ACTIVE", "ENDED", "REDEEMED"]);
-
-export default StreamStatus;
