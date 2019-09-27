90623fda0b4660eb15a904c2bce2280117373911
Paul Razvan Berg <hello@paulrberg.com>
2019-09-27 04:19:45 +0300

    chore: temporarily remove truffle migrations

--- packages/protocol/migrations/3_deploy_ctoken_manager.js
+++
@@ -1,7 +0,0 @@
-/* global artifacts */
-const CTokenManager = artifacts.require("./CTokenManager.sol");
-
-module.exports = (deployer, _, accounts) => {
-  const opts = { from: accounts[0] };
-  deployer.deploy(CTokenManager, opts);
-};
